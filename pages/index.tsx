8095cc7f10c9aa533e7c1c0758260f037e9be4a3
Richie Yi <helloworldrichie@gmail.com>
2022-09-16 16:57:20 -0700

    Add hero image and text to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div />;
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-2xl mt-8 mb-2">Todo Lists made easy!</h1>
+      <img src="/hero.jpg" />
+      <span>Create an account to start today.</span>
+    </div>
+  );
 };
 
 export default Home;
