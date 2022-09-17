3398c88afbde83e1a7ffb53b595f97e0b2ded9ef
Richie Yi <helloworldrichie@gmail.com>
2022-09-17 08:30:47 -0700

    Add react-toastify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 import { ApolloProvider } from '@apollo/client';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { UserProvider } from '../context/userContext';
 import apolloClient from '../lib/apollo';
 import Layout from '../components/Layout';
@@ -11,11 +13,14 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ApolloProvider client={apolloClient}>
       <UserProvider>
         <Layout>
-          <Component {...pageProps} />
+          <>
+            <Component {...pageProps} />
+            <ToastContainer />
+          </>
         </Layout>
       </UserProvider>
     </ApolloProvider>
   );
 }
 
-export default MyApp
+export default MyApp;
